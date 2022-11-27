ff207b39f8323e06d57d50d31b6f80b02b39cb40
fsps60312 <fsps60312@yahoo.com.tw>
2022-11-28 00:34:11 +0800

    first commit

---
+++ build/launch-playground-MinSizeRel.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/fsps6/OneDrive - csie.ntu.edu.tw/Sync/Visual C++ Code/CSRT/playground/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\fsps6\OneDrive - csie.ntu.edu.tw\Sync\Visual C++ Code\CSRT\build\MinSizeRel\playground.exe"  
+pause
